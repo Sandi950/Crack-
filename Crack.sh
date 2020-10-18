761e55c8a00935fb6414c21b61b42d7ec9e561f2
Hacker Gaming <69993535+Sandi950@users.noreply.github.com>
2020-10-18 23:55:08 +0700

    CRACK

---
+++ Crack.sh
@@ -0,0 +1,37 @@
+#usr/bin/bash
+
+clear
+
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+ab='\e[0;37;1m' #abu
+sleep 1
+echo "
+  _________                    .___. __
+ /   _____/ ____    ____     __| _/ |__|
+ \_____  \ \__  \  /    \   / __ |  |  |
+ /        \/ __ \ |   |  \ / /_/ |  |  |
+/_______  (____  /___ |  / \____ |  |__|
+        \/     \/      \/       \/ " | lolcat
+echo
+echo $me"           (Dark Fb No Login)"
+echo
+echo "=======================[]===================" | lolcat
+echo $me"•          [ Author : Sandi950 ]          •"
+echo $pu"•         [ YouTube : Sandi Yt  ]         •"
+echo "=======================[]===================" | lolcat
+echo
+echo $cy"Tulis (pass.txt)"
+echo
+echo -n $i"list txt : " ;read aj
+echo
+echo
+echo $i"Crack Runing....."
+echo "=============================================" | lolcat
+sleep 50
+sh psw.sh
