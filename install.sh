bc67beb05609ef44635342f90b856af64ff6949a
Hacker Gaming <69993535+Sandi950@users.noreply.github.com>
2020-10-19 00:00:12 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,17 @@
+clear
+pkg update && pkg upgrade
+pkg install python
+pkg install python2
+pkg install git
+pkg install figlet
+pkg install toilet
+pkg install lolcat
+pkg install ruby
+gem install lolcat
+pkg install unzip
+pkg install apche2
+pip install mechanize
+pip2 install requests
+pip install requests futures bs4
+clear
+sh Crack.sh
