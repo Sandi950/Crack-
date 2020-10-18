225d1e8c3738384e160a6fc1860e9c9c5dc0b418
Hacker Gaming <69993535+Sandi950@users.noreply.github.com>
2020-10-18 23:55:54 +0700

    Add files via upload

---
+++ psw.sh
@@ -0,0 +1,170 @@
+#usr/bin/bash
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+ab='\e[0;37;1m' #abu
+sleep 5
+echo $ku"[Cekpoint] 100041624616674 | Ahmad123"
+sleep 5
+echo $i"[Berhasil] 100041457253190 | Sayang"
+sleep 5
+echo $i"[Berhasil] 100041695324051 | Sayang"
+sleep 5
+echo $i"[Berhasil] 100041244861619 | Sayang"
+sleep 5
+echo $ku"[Cekpoint] 100041299883223 | Eka12345"
+sleep 5
+echo $ku"[Cekpoint] 100041207390449 | Fitri123"
+sleep 5
+echo $i"[Berhasil] 100041236310921 | Sayang"
+sleep 5
+echo $ku"[Cekpoint] 100031827231168 | Sayang"
+sleep 5
+echo $ku"[Cekpoint] 100041450173633 | Adit123"
+sleep 5
+echo $ku"[Cekpoint] 100041418106236 | Yohanes123"
+sleep 5
+echo $i"[Berhasil] 100041203311131 | Sayang"
+sleep 5
+echo $i"[Berhasil] 100041451694623 | Anjing"
+sleep 5
+echo $ku"[Cekpoint] 100041668711787 | Rendi12"
+sleep 5
+echo $ku"[Cekpoint] 100037831518094 | Bangsa"
+sleep 5
+echo $i"[Berhasil] 100041645791814 | Anjing"
+sleep 5
+echo $ku"[Cekpoint] 100041297749278 | Pipit123"
+sleep 5
+echo $ku"[Cekpoint] 100001540824471 | Bangsat"
+sleep 5
+echo $ku"[Cekpoint] 100041645225376 | Rendy123"
+sleep 5
+echo $i"[Berhasil] 100041559543830 | Bangsat"
+sleep 5
+echo $i"[Berhasil] 100041736991900 | Sayang"
+sleep 5
+echo $i"[Berhasil] 100041657493617 | Sayang"
+sleep 5
+echo $i"[Berhasil] 100041350370019 | Kontol"
+sleep 5
+echo $i"[Berhasil] 100041256223018 | Anjing"
+sleep 5
+echo $ku"[Cekpoint] 100041301347926 | Cibro123"
+sleep 5
+echo $i"[Berhasil] 100041380007858 | Kontol"
+sleep 5
+echo $i"[Berhasil] 100041598188654 | Sayang"
+sleep 5
+echo $i"[Berhasil] 100041639732724 | Kontol"
+sleep 5
+echo $ku"[Cekpoint] 100030082039726 | Bangsat"
+sleep 5
+echo $ku"[Cekpoint] 100040272319936 | Putra123"
+sleep 5
+echo $ku"[Cekpoint] 100041577787659 | Galang123"
+sleep 5
+echo $i"[Berhasil] 100041358288881 | Sayang"
+sleep 5
+echo $i"[Berhasil] 100041436166411 | Sayang"
+sleep 5
+echo $i"[Berhasil] 100041530002804 | Anjing"
+sleep 5
+echo $i"[Berhasil] 100041547638023 | Kontol"
+sleep 5
+echo $ku"[Cekpoint] 100033892520489 | Kontol"
+sleep 5
+echo $i"[Berhasil] 100041616906741 | Sayang"
+sleep 5
+echo $i"[Berhasil] 100041456264660 | Anjing"
+sleep 5
+echo $i"[Berhasil] 100041335182474 | Sayang"
+sleep 5
+echo $i"[Berhasil] 100041687940590 | Sayang"
+sleep 5
+echo $i"[Berhasil] 100041497363501 | Enii123"
+sleep 5
+echo $ku"[Cekpoint] 100041671322614 | Vita123"
+sleep 5
+echo $ku"[Cekpoint] 100041600377889 | Babeh123"
+sleep 5
+echo $ku"[Cekpoint] 100025494649116 | Nanda123"
+sleep 5
+echo $i"[Berhasil] 100041436226423 | Kontol"
+sleep 5
+echo $ku"[Cekpoint] 100041346344656 | Putra123"
+sleep 5
+echo $ku"[Cekpoint] 100036659900376 | Kontol"
+sleep 5
+echo $ku"[Cekpoint] 100041426257579 | Susan123"
+sleep 5
+echo $ku"[Cekpoint] 100041672132566 | Surya123"
+sleep 5
+echo $i"[Berhasil] 100041666311344 | Kontol"
+sleep 5
+echo $i"[Berhasil] 100041610155338 | Anjing"
+sleep 5
+echo $i"[Berhasil] 100041601904927 | Nomi123"
+sleep 5
+echo $ku"[Cekpoint] 100041667961822 | Sofi123"
+sleep 5
+echo $ku"[Cekpoint] 100041625994941 | Fauzi123"
+sleep 5
+echo $ku"[Cekpoint] 100041486861225 | Vildi123"
+sleep 5
+echo $i"[Berhasil] 100041529573616 | Sayang"
+sleep 5
+echo $i"[Berhasil] 100041215429748 | Anjing"
+sleep 5
+echo $i"[Berhasil] 100041391587159 | Sayang"
+sleep 5
+echo $i"[Berhasil] 100041364581384 | Sayang"
+sleep 5
+echo $i"[Berhasil] 100041404574404 | Sayang"
+sleep 5
+echo $i"[Berhasil] 100041359456125 | Kontol"
+sleep 5
+echo $ku"[Cekpoint] 100041694211602 | Iksan12"
+sleep 5
+echo $i"[Berhasil] 100041253020527 | Kontol"
+sleep 5
+echo $ku"[Cekpoint] 100041237673987 | Nafa12345"
+sleep 5
+echo $i"[Berhasil] 100041201932125 | Anjing"
+sleep 5
+echo $ku"[Cekpoint] 100041348537306 | Apan123"
+sleep 5
+echo $ku"[Cekpoint] 100041669341168 | Umam123"
+sleep 5
+echo $i"[Berhasil] 100041150777641 | Sayang"
+sleep 5
+echo $ku"[Cekpoint] 100034437255097 | Anjing"
+sleep 5
+echo $ku"[Cekpoint] 100041282092710 | Hendra123"
+sleep 5
+echo $ku"[Cekpoint] 100036692083814 | Riyadi123"
+sleep 5"
+echo $i"[Berhasil] 100041425094523 | Meta123"
+sleep 5
+echo $i"[Berhasil] 100041423323363 | Sayang"
+sleep 5
+echo $i"[Berhasil] 100041315180719 | Sayang"
+sleep 5
+echo $ku"[Cekpoint] 100041336651887 | Yoga12345"
+sleep 5
+echo $i"[Berhasil] 100041273808068 | Sayang"
+sleep 5
+echo $i"[Berhasil] 100041435263291 | Sayang"
+sleep 10
+echo "=============================================" | lolcat
+echo $i"[✓] Selesai..."
+echo $i"[+] Total CP 35"
+echo $i"[+] Total OK 42"
+sleep 2
+echo
+echo $bi"[kembali]"
+
